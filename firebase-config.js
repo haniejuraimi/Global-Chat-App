5c2a0e91dbfff054410dba0b6192880607d9c112
haniejuraimi <haniealia08@gmail.com>
2019-09-07 11:22:18 +0800

    Completed Global Chat

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBWXVcvbPJP79itaOlicdnQc3p26C7o2C4",
+    authDomain: "global-chat-live-ba88a.firebaseapp.com",
+    databaseURL: "https://global-chat-live-ba88a.firebaseio.com",
+    projectId: "global-chat-live-ba88a",
+    storageBucket: "",
+    messagingSenderId: "196687366926",
+    appId: "1:196687366926:web:6a08eb2598183eccdba55f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var db = firebase.firestore();
--- main.js
+++ main.js
@@ -1,8 +1,56 @@
 var app = new Vue({
     el: '#app',
     data: {
-      messages: [
-          
-      ]
+      messages: [],
+      // the enter bar where you type stuff and press enter to the global chat 
+      newMessage:"",
+      unsavedUsername:"",
+      username:"",
+    },
+    created: function(){
+      db.collection("message")
+      .orderBy('date', 'asc')
+      .onSnapshot(messagesCollection => {
+        messagesCollection.forEach(messageItem => {
+          this.message.push(messageItem.data());
+        })
+      })
+    },
+    methods: {
+      logout: function(){
+        this.username = ''
+      },
+      saveUsername: function(){
+        this.username = this.unsavedUsername;
+      },
+      resetMessageInput: function(){
+        this.newMessage = '';
+      }, 
+
+      enterNewMessage: function(){
+        const newMessage = {
+          message: this.newMessage,
+          date: new Date(),
+          username: this.username,
+          }
+
+        // accessing everything in messages and newMessage
+        // pushing what is in newMessage to the messages 
+        this.messages.push(newMessage);
+
+        this.addToFirestore(newMessage);
+         // clearing put the input enter space place where you type your messages
+        this.newMessage ="";
+      },
+      addToFirestore: function(newMessage){
+        db.collection('messages')
+          .add(newMessage)
+          .then(function(documentId){
+            console.log("Document has been inserted", documentId);
+          })
+          .catch(function(error){
+            console.log(error);
+          })
+      }
     }
   })
